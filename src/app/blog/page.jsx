fa4bcda82b4fdb7285f737c46105a3320ac5accd
JewelHFahim <jewelhfahim@gmail.com>
2023-10-01 22:07:09 +0600

    bug fixed on blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,20 +8,20 @@ export const metadata = {
   description: "From Blog Page",
 };
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/posts", {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 const Blog = async () => {
-  const data = await getData();
+//   const data = await getData();
 
   return (
     // <div className={styles.mainContainer}>
