cf4c6a1656b32fbb69cc3e3203a9899a841dd078
JewelHFahim <jewelhfahim@gmail.com>
2024-01-18 22:32:18 +0600

    project added

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,29 +7,38 @@ export const metadata = {
 };
 
 const Projects = () => {
-  
   return (
     <div className={styles.container}>
-
       <h1 className={styles.mainTitle}>Projects</h1>
 
       {/* Frontend Projects */}
-      <h1 className={styles.selectTitle}>- Frontend</h1>
+      {/* <h1 className={styles.selectTitle}>- Frontend</h1> */}
 
       <div className={styles.items}>
-        <Link href="https://spagreen.netlify.app/" target="_blank" className={styles.item}>
+        <Link
+          href="https://epickmovies.online/"
+          target="_blank"
+          className={styles.item}
+        >
+          <span className={styles.title}>EpicMovies</span>
+        </Link>
+
+        <Link
+          href="https://spagreen.netlify.app/"
+          target="_blank"
+          className={styles.item}
+        >
           <span className={styles.title}>SpaGreen</span>
         </Link>
 
-        <Link href="https://nature-hub.netlify.app/" target="_blank" className={styles.item}>
+        <Link
+          href="https://nature-hub.netlify.app/"
+          target="_blank"
+          className={styles.item}
+        >
           <span className={styles.title}>Nature Hub</span>
         </Link>
-
-        <Link href="/portfolio/applications" className={styles.item}>
-          <span className={styles.title}>Applications</span>
-        </Link>
       </div>
-
     </div>
   );
 };
